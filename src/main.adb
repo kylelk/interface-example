31c03851cb294efb8fe95dd0a920d9205fb51546
kyle kersey <kylelk@comcast.net>
2016-04-21 10:25:22 -0700

    adding new package

--- src/main.adb
+++ src/main.adb
@@ -3,8 +3,21 @@ with KV_Store;
 with Hash_Store;
 
 procedure Main is
-
    package TIO renames Ada.Text_IO;
+
+   generic
+      type Data_Store is private;
+   package Items is
+      type Status is tagged record
+         Values : Data_Store;
+      end record;
+
+      procedure Set(Self : in out Status; Key, Value : String);
+   end Items;
+
+   package body Items is
+   begin
+      end Items;
 begin
    TIO.Put_Line ("hello world");
 end Main;
