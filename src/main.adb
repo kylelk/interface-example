d4454f9a232295465d0ebdd946f9989a9396b995
kyle kersey <kylelk@comcast.net>
2016-04-21 13:11:01 -0700

    fixed package

--- src/main.adb
+++ src/main.adb
@@ -6,21 +6,21 @@ procedure Main is
    package TIO renames Ada.Text_IO;
 
    generic
-      type Data_Store is interface and KV_Store.KV_Container;
+      type Data_Store is new KV_Store.KV_Container with private;
    package Items is
-      type Status is abstract new Data_Store with Record
-           Saved : Boolean;
-      end record;
-
-      procedure New_Item(Self : in out Status; Key, Value : String);
+      procedure New_Item(Self : in out Data_Store; Key, Value : String);
    end Items;
 
    package body Items is
-      procedure New_Item(Self : in out Status; Key, Value : String) is
+      procedure New_Item(Self : in out Data_Store; Key, Value : String) is
       begin
          Self.Set(Key, Value);
       end New_Item;
    end Items;
+
+
 begin
+
+
    TIO.Put_Line ("hello world");
 end Main;
