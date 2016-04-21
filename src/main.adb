caeeffd6b56f1fb655ff4aa4f4518d7005b5f575
kyle kersey <kylelk@comcast.net>
2016-04-21 13:27:22 -0700

    fixed example

--- src/main.adb
+++ src/main.adb
@@ -9,6 +9,7 @@ procedure Main is
       type Data_Store is new KV_Store.KV_Container with private;
    package Items is
       procedure New_Item(Self : in out Data_Store; Key, Value : String);
+      function Get_Item(Self : in out Data_Store; Key : String) return String;
    end Items;
 
    package body Items is
@@ -16,11 +17,16 @@ procedure Main is
       begin
          Self.Set(Key, Value);
       end New_Item;
+
+      function Get_Item(Self : in out Data_Store; Key : String) return String is
+      begin
+         return Self.Get(Key);
+      end Get_Item;
    end Items;
 
+   Data : Hash_Store.Data;
    package Client is new Items(Hash_Store.Data);
 begin
-
-
-   TIO.Put_Line ("hello world");
+   Client.New_Item(Data, "message", "hello world");
+   TIO.Put_Line(Client.Get_Item(Data, "message"));
 end Main;
