690df2e5fab90d68974baca63e1eb429abeb9c33
kyle kersey <kylelk@comcast.net>
2016-05-03 08:15:09 -0700

    added record to items packages for status

--- src/main.adb
+++ src/main.adb
@@ -8,45 +8,82 @@ procedure Main is
 
    package Items is
       use KV_Store;
-      procedure Init(Data : in out KV_Interface'Class);
-      procedure Done(Data : in out KV_Interface'Class);
-      procedure New_Item(Data : in out KV_Interface'Class; Key, Value : String);
-      function Get_Item(Data : in out KV_Interface'Class; Key : String) return String;
+      type Status is tagged record
+         Items_Inserted : Integer := 0;
+         Items_Remove   : Integer := 0;
+      end record;
+
+      procedure Init (Info : in out Status; Data : in out KV_Interface'Class);
+
+      procedure Done (Info : in out Status; Data : in out KV_Interface'Class);
+
+      procedure New_Item
+        (Info       : in out Status;
+         Data       : in out KV_Interface'Class;
+         Key, Value :        String);
+
+      function Get_Item
+        (Info : in out Status;
+         Data : in out KV_Interface'Class;
+         Key  :        String) return String;
    end Items;
 
    package body Items is
-      procedure Init(Data : in out KV_Interface'Class) is
+      procedure Init
+        (Info : in out Status;
+         Data : in out KV_Interface'Class)
+      is
       begin
          Data.Setup;
       end Init;
 
-      procedure Done(Data : in out KV_Interface'Class) is
+      procedure Done
+        (Info : in out Status;
+         Data : in out KV_Interface'Class)
+      is
       begin
          Data.Cleanup;
       end Done;
 
-      procedure New_Item(Data : in out KV_Interface'Class; Key, Value : String) is
+      procedure New_Item
+        (Info       : in out Status;
+         Data       : in out KV_Interface'Class;
+         Key, Value :        String)
+      is
       begin
-         Data.Set(Key, Value);
+         if not Data.Contains (Key) then
+            Info.Items_Inserted := Info.Items_Inserted + 1;
+         end if;
+         Data.Set (Key, Value);
       end New_Item;
 
-      function Get_Item(Data : in out KV_Interface'Class; Key : String) return String is
+      function Get_Item
+        (Info : in out Status;
+         Data : in out KV_Interface'Class;
+         Key  :        String) return String
+      is
       begin
-         return Data.Get(Key);
+         return Data.Get (Key);
       end Get_Item;
    end Items;
 
-   Data : Hash_Store.Data;
+   Data   : Hash_Store.Data;
+   Client : Items.Status;
 begin
    -- initalize the data store
-   Items.Init(Data);
+   --Items.Init(Status, Data);
+   Client.Init (Data);
 
-   -- create a new item
-   Items.New_Item(Data, "test", "hello world");
+   for I in 1 .. 10 loop
+      -- create a new item
+      Client.New_Item (Data, "test" & I'Img, "item" & I'Img);
 
-   -- get an item by it's key name
-   TIO.Put_Line(Items.Get_Item(Data, "test"));
+      -- get an item by it's key name
+      TIO.Put_Line (Client.Get_Item (Data, "test" & I'Img));
+   end loop;
+
+   TIO.Put_Line ("inserted " & Client.Items_Inserted'Img & " new items");
 
    -- close the data store
-   Items.Done(Data);
+   Client.Done (Data);
 end Main;
